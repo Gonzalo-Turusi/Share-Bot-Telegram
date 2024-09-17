3380560944ae6e7733a0dec91c1c2e8d2d23a4c2
Gonzalo-Turusi <gonzalo.turusi@gmail.com>
2024-09-17 08:59:54 -0300

    Update FetchShareCommand.py

--- commands/FetchShareCommand.py
+++ commands/FetchShareCommand.py
@@ -3,7 +3,9 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from configuration.ConfigEnv import configEnv
 from managers.DBManagerSQLLite import DBManagerSQLLite
-from shared.Price_formatter import format_price_usd
+from shared.Price_formatter import format_price_usd, format_price_arg
+from shared.GetDolarMep import GetDolarMep
+from shared.CurrencyConverter import CurrencyConverter
 
 class FetchShareCommand:
 	def __init__(self):
@@ -26,15 +28,30 @@ class FetchShareCommand:
 				low_price = format_price_usd(data['04. low'].iloc[0])
 				previous_close = format_price_usd(data['08. previous close'].iloc[0])
 				
+				dolarMep = GetDolarMep().get_dolar()
+
+				if dolarMep == None:
+					raise Exception("No se pudo obtener el valor del MEP.")
+				
+				currency_converter = CurrencyConverter(dolarMep)
+
+				formatted_dolarmep = format_price_arg(str(dolarMep))
+				formatted_price_arg = format_price_arg(str(currency_converter.dollars_to_pesos(float(data['05. price'].iloc[0]))))
+				formatted_high_arg = format_price_arg(str(currency_converter.dollars_to_pesos(float(data['03. high'].iloc[0]))))
+				formatted_low_arg = format_price_arg(str(currency_converter.dollars_to_pesos(float(data['04. low'].iloc[0]))))
+				formatted_open_arg = format_price_arg(str(currency_converter.dollars_to_pesos(float(data['02. open'].iloc[0]))))
+				formatted_previous_close_arg = format_price_arg(str(currency_converter.dollars_to_pesos(float(data['08. previous close'].iloc[0]))))
+				
 				response_message = (
 					f"Información de {share_code}:\n"
-					f"Precio Actual: {price}\n"
-					f"Apertura: {open_price}\n"
-					f"Máximo: {high_price}\n"
-					f"Mínimo: {low_price}\n"
-					f"Cierre Anterior: {previous_close}\n"
+					f"Precio Actual: {price} / {formatted_price_arg}\n"
+					f"Apertura: {open_price} / {formatted_open_arg}\n"
+					f"Máximo: {high_price} / {formatted_high_arg}\n"
+					f"Mínimo: {low_price} / {formatted_low_arg}\n"
+					f"Cierre Anterior: {previous_close} / {formatted_previous_close_arg}\n"
                     f"Volumen: {data['06. volume'].iloc[0]}\n"
                     f"Porcentaje de Cambio: {data['10. change percent'].iloc[0]}\n"
+					f"\nLa acción cuesta {price}, con un MEP a {formatted_dolarmep}, me daría un precio de {formatted_price_arg}."
 				)
 			else:
 				response_message = f"No se encontró información para {share_code}. Asegúrate de que el símbolo es correcto y vuelve a intentarlo."
